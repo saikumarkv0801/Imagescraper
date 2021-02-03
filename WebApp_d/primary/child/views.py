3ade8bd113fec393bf1982c3848fb1441bd41313
sai kumar <saikumarkv99@gmail.com>
2021-02-03 10:29:52 +0530

    init

---
+++ WebApp_d/primary/child/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect,get_object_or_404, render_to_response
+from math import ceil
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+from django.http import HttpResponse
+from django.db import models
+from django.contrib import messages
+
+def index(request):
+    return HttpResponse("hello world")
